70954ba10198b88762319ba3094c0c8cdda95c6e
Anton <kodvmeshke@gmail.com>
2022-06-27 21:15:05 +0300

    Part 2 done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 $(function(){
   
-  $('.bike-slider').slick({
+  $(".bike-slider, .slider__items").slick({
     arrows: false,
     dots: true,
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000,
   });
+
+    //https://webcomplex.com.ua/jquery/plavnyj-skroll-posle-nazhatiya-na-yakornuyu-ssylku.html
+  $(".menu, .bike__column").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 
 });
